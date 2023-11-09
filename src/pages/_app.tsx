7f7bd5ecff3030469ec55989ef5a90dc511cefd2
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2023-11-08 18:39:48 -0600

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+//for redux
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import store, {persistor} from '@/store/store'
+import Layout from '@/components/Layout'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+		<Provider store={store}>
+			<PersistGate persistor={persistor} loading={null}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</PersistGate>
+		</Provider>
+  );
 }
